8181963baa5c27f8af040f1a967c54b1f0bc1d9c
TheBurchLog <5104941+TheBurchLog@users.noreply.github.com>
2021-02-22 10:53:51 -0500

    switching over to javascript

---
+++ index.js
@@ -0,0 +1,23 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+
+  const tag = core.getInput('tag');
+  const env_variable = core.getInput('env-variable');
+  const default_value = core.getInput('default-value');
+  const tag_position = core.getInput('tag-position');
+
+  const issue_body = github.event.issue.body;
+  const re = new RegExp("(?<=" + tag + "\\s)(\\w+)");
+
+  const tags = re.exec(issue_body);
+
+  value = default_value
+
+  core.exportVariable(env_variable, value);
+
+  console.log(`The event payload: ${value}`);
+} catch (error) {
+  core.setFailed(error.message);
+}
