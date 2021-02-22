3ae4f936826f5e92a1aeefe8f58f40c6daa5185e
TheBurchLog <5104941+TheBurchLog@users.noreply.github.com>
2021-02-22 13:40:58 -0500

    Better handling for no tag found

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ try {
   const default_value = core.getInput('default-value');
   const tag_position = core.getInput('tag-position');
 
+  var value = default_value;
+
+  var issue_body = null;
+
   // Find the correct body obj
   if ('issue' in github.context.payload){
     var issue_body = github.context.payload.issue.body;
@@ -15,46 +19,47 @@ try {
   else if ('pull_request' in github.context.payload){
     var issue_body = github.context.payload.pull_request.body;
   }
-  else{
-    core.exportVariable(env_variable, default_value);
-    return;
-  }
-  // Remove line breaks
-  issue_body = issue_body.replace(/(\r\n|\n|\r)/gm, " ");
 
-  // Regex wasn't working, so we have to manually do the look up
-  const body_words = issue_body.split(" ");
+  if (issue_body != null){
 
-  var i;
-  var tags_found = 0;
-  for (i = 0; i < body_words.length; i++){
-    if (body_words[i] == tag){
-        if ((i + 1) < body_words.length){
-            if (tag_position == -1){
-              value = body_words[i + 1];
+      // Remove line breaks
+      issue_body = issue_body.replace(/(\r\n|\n|\r)/gm, " ");
 
-              // Handle Tables
-              if (value == "|" && (i + 3) < body_words.length && body_words[i+3] == "|"){
-                value = body_words[i + 2];
-              }
+      // Regex wasn't working, so we have to manually do the look up
+      const body_words = issue_body.split(" ");
+
+      var i;
+      var tags_found = 0;
+      for (i = 0; i < body_words.length; i++){
+        if (body_words[i] == tag){
+            if ((i + 1) < body_words.length){
+                if (tag_position == -1){
+                  value = body_words[i + 1];
+
+                  // Handle Tables
+                  if (value == "|" && (i + 3) < body_words.length && body_words[i+3] == "|"){
+                    value = body_words[i + 2];
+                  }
+                }
+                else if (tags_found == tag_position){
+                  value = body_words[i + 1];
+
+                  // Handle Tables
+                  if (value == "|" && (i + 3) < body_words.length && body_words[i+3] == "|"){
+                    value = body_words[i + 2];
+                  }
+                  break;
+                }
+                tags_found++;
             }
-            else if (tags_found == tag_position){
-              value = body_words[i + 1];
-
-              // Handle Tables
-              if (value == "|" && (i + 3) < body_words.length && body_words[i+3] == "|"){
-                value = body_words[i + 2];
-              }
-              break;
-            }
-            tags_found++;
         }
-    }
+      }
   }
 
   core.exportVariable(env_variable, value);
 
   console.log(`The Tag Value: ${value}`);
+  }
 } catch (error) {
   core.setFailed(error.message);
 }
