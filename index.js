9068832806dd133ad21f2818ef97a81afeb48577
TheBurchLog <5104941+TheBurchLog@users.noreply.github.com>
2021-02-22 11:19:29 -0500

    Adding Loop

--- index.js
+++ index.js
@@ -9,14 +9,16 @@ try {
   const tag_position = core.getInput('tag-position');
 
   const context = JSON.stringify(github.context, undefined, 2)
-  // console.log(`The event payload: ${context}`);
+  console.log(`The context payload: ${context}`);
 
-  const issue_body = github.context.issue.body;
-  console.log(`The event payload: ${issue_body}`);
+  const issue_body = context["issue"]["body"];
+  console.log(`The issue payload: ${issue_body}`);
 
   const re = new RegExp("(?<=" + tag + "\\s)(\\w+)");
 
   const tags = re.exec(issue_body);
+  console.log(`The tags: ${tags}`);
+
 
   var value = default_value;
   var i;
